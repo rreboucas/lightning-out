541f2559ab739b1678b015150e351e3c723c8efc
rorofit <rreboucas@rodrigo-reb-ltm.internal.salesforce.com>
2016-01-13 17:30:45 -0500

    added cor32

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
+  express.static(__dirname + '/public');
 });
 
 app.get('/', function(req, res) {
