e9974fc13d506d5c55b729f4e756e36de6056f98
rorofit <rreboucas@rodrigo-reb-ltm.internal.salesforce.com>
2016-01-13 17:26:34 -0500

    added cors

--- server.js
+++ server.js
@@ -15,8 +15,14 @@ var port = process.env.PORT || 5000;
 var https_port = process.env.HTTPS_PORT || parseInt(port) + 1;
 
 app.set('view engine', 'ejs');
-
+//
 app.use(express.static(__dirname + '/public'));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', function(req, res) {
 	res.render('index', {appId: process.env.APPID, loApp: process.env.LOAPP});
